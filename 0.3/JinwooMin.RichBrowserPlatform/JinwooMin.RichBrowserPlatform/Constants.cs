7e271803a4d14f460614c7a72abbcba1f1063c2f
yeoupooh <yeoupooh@87b7fced-2a2d-0410-be69-550ccd6fe031>
2007-07-30 00:09:32 +0000

    RichBrowserPlatform 0.2.18 released.

--- 0.3/JinwooMin.RichBrowserPlatform/JinwooMin.RichBrowserPlatform/Constants.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace JinwooMin.RichBrowserPlatform
-{
-    public enum PluginActionResult { None, Success, Fail, Cancel };
-}
