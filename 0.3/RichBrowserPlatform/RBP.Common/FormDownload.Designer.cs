c57bfbec6becf2bb2ea6a83c2faeec8d347cc15a
yeoupooh <yeoupooh@87b7fced-2a2d-0410-be69-550ccd6fe031>
2007-10-27 00:35:50 +0000

    - added RBP.Common.

---
+++ 0.3/RichBrowserPlatform/RBP.Common/FormDownload.Designer.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * SharpDevelop으로 작성되었습니다.
+ * 사용자: yeoupooh
+ * 날짜: 2007-10-27
+ * 시간: 오전 9:28
+ * 
+ * 이 템플리트를 변경하려면 [도구->옵션->코드 작성->표준 헤더 편집]을 이용하십시오.
+ */
+namespace RBP.Common
+{
+	partial class FormDownload
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// FormDownload
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Text = "FormDownload";
+			this.Name = "FormDownload";
+		}
+	}
+}
---
+++ 0.3/RichBrowserPlatform/RBP.Common/FormDownload.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * SharpDevelop으로 작성되었습니다.
+ * 사용자: yeoupooh
+ * 날짜: 2007-10-27
+ * 시간: 오전 9:28
+ * 
+ * 이 템플리트를 변경하려면 [도구->옵션->코드 작성->표준 헤더 편집]을 이용하십시오.
+ */
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RBP.Common
+{
+	/// <summary>
+	/// Description of FormDownload.
+	/// </summary>
+	public partial class FormDownload : Form
+	{
+		public FormDownload()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
