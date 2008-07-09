10662c23f8bed0bab7cfcc309daf5e715e59b43e
yeoupooh <yeoupooh@87b7fced-2a2d-0410-be69-550ccd6fe031>
2008-07-09 03:09:44 +0000

    * fixed ShowAction handling.  * added source comments.

--- 0.3/RichBrowserPlatform/OpenCS.RBP/Actions/ShowAction.cs
+++ 0.3/RichBrowserPlatform/OpenCS.RBP/Actions/ShowAction.cs
@@ -5,19 +5,29 @@ using OpenCS.Common.Action;
 
 namespace OpenCS.RBP.Actions
 {
+    /// <summary>
+    /// 객체를 표시하라는 액션
+    /// </summary>
     public class ShowAction : IAction
     {
-        private object mObject;
+        private object mShowingObject;
 
-        public object Object
+        /// <summary>
+        /// 표시할 객체를 가져오거나 설정한다.
+        /// </summary>
+        public object ShowingObject
         {
-            get { return mObject; }
-            set { mObject = value; }
+            get { return mShowingObject; }
+            set { mShowingObject = value; }
         }
 
-        public ShowAction(object obj)
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        /// <param name="showingObject">표시할 객체</param>
+        public ShowAction(object showingObject)
         {
-            mObject = obj;
+            mShowingObject = showingObject;
         }
     }
 }
--- 0.3/RichBrowserPlatform/OpenCS.RBP/BaseToggleableRbpPlugin.cs
+++ 0.3/RichBrowserPlatform/OpenCS.RBP/BaseToggleableRbpPlugin.cs
@@ -9,30 +9,46 @@ using OpenCS.RBP.Actions;
 
 namespace OpenCS.RBP
 {
+    /// <summary>
+    /// 토글 가능한 패널 플러그인 기본 구현
+    /// </summary>
     abstract public class BaseToggleableRbpPlugin : BaseRbpPlugin
     {
         private DockContent mDc;
         private ToolStripButton mButton;
         private ToolStripMenuItem mMenuItem;
 
+        /// <summary>
+        /// 도킹 패널을 가져오거나 설정한다.
+        /// </summary>
         public DockContent DockContent
         {
             get { return mDc; }
             set { mDc = value; }
         }
 
+        /// <summary>
+        /// 버튼을 가져오거나 설정한다.
+        /// </summary>
         public ToolStripButton Button
         {
             get { return mButton; }
             set { mButton = value; }
         }
 
+        /// <summary>
+        /// 메뉴아이템을 가져오거나 설정한다.
+        /// </summary>
         public ToolStripMenuItem MenuItem
         {
             get { return mMenuItem; }
             set { mMenuItem = value; }
         }
 
+        /// <summary>
+        /// 토글 관련 리소스를 설정한다.
+        /// IPlugin.Init() 내에서 DockContent를 초기화한 후 호출한다.
+        /// </summary>
         protected void InitToggleableResources()
         {
             mDc.HideOnClose = true;
@@ -44,6 +60,7 @@ namespace OpenCS.RBP
         }
 
         /// <summary>
+        /// 토글 관련 리소스를 설정한다.
         /// IPlugin.Init() 내에서 DockContent를 초기화한 후 호출한다.
         /// </summary>
         protected void InitToggleableResources(Image image)
@@ -57,6 +74,7 @@ namespace OpenCS.RBP
         }
 
         /// <summary>
+        /// 토글 관련 리소스를 해제한다.
         /// IPlugin.Deinit() 내에서 DockContent.Close()하기전에 호출한다.
         /// </summary>
         protected void DeinitToggleableResources()
@@ -65,12 +83,17 @@ namespace OpenCS.RBP
             RichBrowserControl.RemoveMenuItem(mMenuItem);
         }
 
+        /// <summary>
+        /// <c>ShowAction</c>을 처리한다.
+        /// </summary>
+        /// <param name="action">액션</param>
+        /// <returns>성공하면 <c>ActionResult.Success</c></returns>
         protected ActionResult HandleShowAction(IAction action)
         {
             if (action is ShowAction)
             {
-                DockContent dc = (action as ShowAction).Object as DockContent;
-                if (dc != null)
+                DockContent dc = (action as ShowAction).ShowingObject as DockContent;
+                if (dc != null && dc == mDc)
                 {
                     ShowDockContent();
 
@@ -81,6 +104,9 @@ namespace OpenCS.RBP
             return ActionResult.NotHandled;
         }
 
+        /// <summary>
+        /// 도킹 패널을 표시한다.
+        /// </summary>
         abstract protected void ShowDockContent();
     }
 }
